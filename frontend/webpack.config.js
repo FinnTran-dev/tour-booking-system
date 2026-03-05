dcf085c3a9295d6634748a1833cc2635f96b69a1
Finn Tran <hieu.trantrung@adamosoft.com>
2026-03-06 06:17:24 +0700

    Fix build project

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,58 +3,58 @@ const { VueLoaderPlugin } = require('vue-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/[name].[contenthash:8].js',
-    publicPath: '/',
-    clean: true,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].[contenthash:8].js',
+        publicPath: '/',
+        clean: true,
     },
-    extensions: ['.js', '.vue', '.json'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
         },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      title: 'Tour Booking System',
-    }),
-  ],
-  devServer: {
-    port: 3000,
-    hot: true,
-    historyApiFallback: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      },
+        extensions: ['.js', '.vue', '.json'],
     },
-  },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            title: 'Tour Booking System',
+        }),
+    ],
+    devServer: {
+        port: 3000,
+        hot: true,
+        historyApiFallback: true,
+        proxy: {
+            '/api': {
+                target: 'http://backend:8000',
+                changeOrigin: true,
+            },
+        },
+    },
 };
